b1555297c12cb34c2662a238239f0b0c83bbc604
Alexandru Cara <alexandru.cara.2@gmail.com>
2025-10-22 17:30:40 -0400

    Nice looking mobile and desktop I think

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 	<div class="home-grid relative z-10 h-screen md:gap-2 md:p-8">
 		<div class="cell relative md:col-span-2 md:row-span-2">
 			<CustomButton onclick={() => sphereColor.set('#b16286')} id="intro" href="/about">
-				<button class="latte bg-gruv-purple-dim h-full w-full">
+				<button class="latte bg-gruv-purple-dim interior-button">
 					<p class="font-ovo text-5xl font-semibold">alexandru cara</p>
 					<hr class="mx-auto my-4 h-1 w-12 rounded-sm border-0 bg-gray-100 dark:bg-black" />
 					<p class="font-ovo text-5xl font-semibold">software engineer</p>
@@ -20,14 +20,14 @@
 		</div>
 		<div class="cell relative">
 			<CustomButton id="exhibit1" href="/aes" onclick={() => setcolor('#689d6a')}>
-				<button class="latte bg-gruv-aqua-dim h-full w-full">
+				<button class="latte bg-gruv-aqua-dim interior-button">
 					<p class="font-ovo text-4xl font-extrabold">AES</p>
 				</button>
 			</CustomButton>
 		</div>
-		<div class="cell relative w-full">
+		<div class="cell relative">
 			<CustomButton id="exhibit2" href="/leet" onclick={() => setcolor('#d79921')}>
-				<button class="latte bg-gruv-yellow-dim h-full w-full">
+				<button class="latte bg-gruv-yellow-dim interior-button">
 					<p class="font-ovo text-6xl">O(n&#8729lgn)</p>
 					<hr class="mx-auto my-4 h-1 w-12 rounded-sm border-0 bg-gray-100 dark:bg-black" />
 					<p class="font-ovo text-6xl">O(n)</p>
@@ -36,14 +36,14 @@
 		</div>
 		<div class="cell relative md:col-span-2">
 			<CustomButton id="exhibit3" href="/car" onclick={() => setcolor('#dc414f')}>
-				<button class="latte bg-ctp-maroon-600 h-full w-full">
+				<button class="latte bg-ctp-maroon-600 interior-button">
 					<p class="font-ovo text-3xl font-extrabold">rc car</p>
 				</button>
 			</CustomButton>
 		</div>
-		<div class="cell relative w-full">
+		<div class="cell relative">
 			<CustomButton id="blog" href="/blog" onclick={() => setcolor('#7791f5')}>
-				<button class="latte bg-ctp-blue-300 h-full w-full">
+				<button class="latte bg-ctp-blue-300 interior-button">
 					<p class="font-nixie text-3xl font-extrabold">Blog</p>
 				</button>
 			</CustomButton>
@@ -68,6 +68,11 @@
 				'intro intro exhibit2'
 				'exhibit3 exhibit3 blog';
 		}
+
+		.interior-button {
+			height: 100%;
+			width: 100%;
+		}
 	}
 	@media only screen and (width < 48rem) {
 		.home-grid {
@@ -80,9 +85,15 @@
 		}
 
 		.cell {
-			min-height: 19.5vh;
-			max-height: max-content;
+			min-height: 20vh;
+			overflow: scroll;
 			width: 100%;
+		}
+
+		.interior-button {
+			min-height: 100%;
+			min-width: 100%;
+			padding: 3px;
 		}
 	}
 </style>
